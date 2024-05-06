f452b6393635e21d969ab4eb544b29abe5b5d68d
kaiquegostoso <150025044+kaiquegostoso@users.noreply.github.com>
2024-05-06 09:01:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 600);//criar tela
+ background("pink");
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+     
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 40, 40);
+  }
+}
+
